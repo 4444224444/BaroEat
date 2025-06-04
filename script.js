dfacc17791a0787f0ff8b3490f7dee9471c819b2
4444224444 <144399144+4444224444@users.noreply.github.com>
2025-06-04 16:37:39 +0900

    up

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
-const boxes = document.querySelectorAll('.PJG-container > div');
-
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add('visible');
-      } else {
-        entry.target.classList.remove('visible'); // 다시 스크롤 올라가면 제거
-      }
-    });
-  }, { threshold: 0.2 });
-
-  boxes.forEach(box => observer.observe(box));
+  window.addEventListener("scroll", function () {
+    const header = document.getElementById("mainHeader");
+    if (window.scrollY > 50) {
+      header.classList.add("scrolled");
+    } else {
+      header.classList.remove("scrolled");
+    }
+  });
