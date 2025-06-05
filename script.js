4ece7f671f077b99df586dd3611b7fabd39476e1
4444224444 <144399144+4444224444@users.noreply.github.com>
2025-06-05 13:40:44 +0900

    up

--- script.js
+++ script.js
@@ -6,3 +6,17 @@
       header.classList.remove("scrolled");
     }
   });
+
+
+const fallBoxes = document.querySelectorAll('.fall-box');
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      fallBoxes.forEach(box => box.classList.add('active'));
+    } else {
+      fallBoxes.forEach(box => box.classList.remove('active'));
+    }
+  });
+}, { threshold: 0.3 });
+
+observer.observe(document.querySelector('.fall-group'));
